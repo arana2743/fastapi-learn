64a1dd1d970e966a9fc2bf2a287bfb3899913756
arana2743 <abhisekrana777@gmail.com>
2021-03-30 19:13:36 +0530

    Models in FastAPI for post requests

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -22,3 +24,16 @@ def get_blogs(limit: int = 10, published: bool = True, sort: Optional[str] = Non
 @app.get('/blog/{id}')
 def get_blog(id: int):
   return {'data': id} 
+
+class Blog(BaseModel):
+  title: str
+  body: str
+  published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+  return request
+  # return { 'data': 'Blog created successfully' }
+
+if __name__ == '__main__':
+  uvicorn.run(app, host="127.0.0.1", port=9000)
